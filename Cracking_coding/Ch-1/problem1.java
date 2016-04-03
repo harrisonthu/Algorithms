93f030ef1dd72899628179ed963e11c2edd64ff8
harrisonthu <hmt4224@rit.edu>
2016-04-03 02:16:16 -0400

    First Commit

---
+++ Cracking_coding/Ch-1/problem1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class problem1{
+
+	public static void main(String[] args){
+		boolean[] char_set = new boolean[256];
+		for(int i=0; i<char_set.length;i++){
+			System.out.println("char_set["+ i + "]" + char_set[i]);
+		}	
+	}
+
+}
+
+
